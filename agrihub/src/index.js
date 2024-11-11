11f325115e35b0cb057e08b7e1e90429bb586f22
toel-kamanda34 <toelokemwa@gmail.com>
2024-11-12 01:55:05 +0300

    added shopping cart icon

---
+++ agrihub/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <h2>Welcome to our website</h2>
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
