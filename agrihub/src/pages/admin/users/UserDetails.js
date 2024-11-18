4491c0c3a4bc6d70fd340a7f39326f85e687773f
toel-kamanda34 <toelokemwa@gmail.com>
2024-11-18 15:01:39 +0300

    show user details

--- agrihub/src/pages/admin/users/UserDetails.js
+++ agrihub/src/pages/admin/users/UserDetails.js
@@ -1,8 +1,43 @@
-import { useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../../../AppContext";
 
 export default function UserDetails() {
   const [user, setUser] = useState({});
+
+  const params = useParams();
+
+  const { userCredentials, setUserCredentials } = useContext(AppContext);
+
+  const navigate = useNavigate();
+  async function getUserDetails() {
+    try {
+      const response = await fetch("http://localhost:4000/users/" + params.id, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + userCredentials.accessToken,
+        },
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        setUser(data);
+      } else if (response.status === 401) {
+        //unathorized response
+        setUserCredentials(null);
+
+        //redirect to login
+        navigate("/auth/login");
+      } else {
+        alert("Unable to read user details: " + data);
+      }
+    } catch (error) {
+      alert("Unable to connect to the server ");
+    }
+  }
+  useEffect(() => {
+    getUserDetails();
+  }, []);
   return (
     <div className="container my-4">
       <h2 className="mb-3">User Details</h2>
